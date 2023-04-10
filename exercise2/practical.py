4d610954e14e53b23821bd38b06203f0110acb1c
Paripás Viktor <viktor.paripas@kognity.com>
2023-04-10 21:40:54 +0200

    Fix formatting and variable names

--- exercise2/practical.py
+++ exercise2/practical.py
@@ -1,73 +1,82 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[115]:
+# In[ ]:
 
 
-import torch.optim as optim
-from torchvision import models
 import torch
-import torch.nn as nn
+from torch import nn, optim
 from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision import transforms
-from torchvision.datasets import CIFAR10
 from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, models, transforms
 
 
-# In[116]:
+# # Transfer learning on the CIFAR-10 dataset
+
+# In[ ]:
 
 
 # Hyperparameters
-learning_rate = 0.0001
+learning_rate = 1e-4
 batch_size = 50
-num_epochs = 4 #alter this afterwards
+num_epochs = 4  # alter this afterwards
 momentum = 0.9
 loss_function = nn.CrossEntropyLoss()
 
 # Architecture
-num_classes = 10
+NUM_CLASSES = 10
 
 
-# In[117]:
+# In[ ]:
 
 
+RESIZE_SIZE = 70
+CROP_TO_SIZE = 64
 
-train_transforms = transforms.Compose([transforms.Resize((70, 70)),
-                                       transforms.RandomCrop((64, 64)),
-                                       transforms.ToTensor()])
-
-test_transforms = transforms.Compose([transforms.Resize((70, 70)),
-                                      transforms.CenterCrop((64, 64)),
-                                      transforms.ToTensor()])
+transform = transforms.Compose([
+    transforms.Resize((RESIZE_SIZE, RESIZE_SIZE)),
+    transforms.RandomCrop((CROP_TO_SIZE, CROP_TO_SIZE)),
+    transforms.ToTensor(),
+])
 
 
+cifar_10_training_data = datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform)
+cifar_10_test_data = datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform)
 
-train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transforms)
-test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transforms)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+train_loader = DataLoader(
+    cifar_10_training_data, batch_size=batch_size, shuffle=True, num_workers=2
+)
+test_loader = DataLoader(
+    cifar_10_test_data, batch_size=batch_size, shuffle=False, num_workers=2
+)
 
 
 # Training-Testing Functions
 
-# In[118]:
+# In[ ]:
 
 
 writer = SummaryWriter()
 
 
-# In[123]:
+# In[ ]:
 
 
-def train_model(data_loader,network, optimizer, criterion):
+BATCH_TO_PRINT = 100
+
+
+# In[ ]:
+
+
+def train_model(data_loader, network, optimizer, criterion):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     network.to(device)
     network.train()
     for epoch in range(num_epochs):
         running_loss = epoch_loss = 0.
-        for i, data in enumerate(data_loader, 0):
+        for i, data in enumerate(data_loader):
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = network(inputs)
@@ -77,14 +86,17 @@ def train_model(data_loader,network, optimizer, criterion):
             running_loss += loss.item()
             epoch_loss += loss.item()
 
-            if i % 100 == 99:
+            if i % BATCH_TO_PRINT == (BATCH_TO_PRINT - 1):
                 print('[Epoch %d, Batch %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 100))
+                      (epoch + 1, i + 1, running_loss / BATCH_TO_PRINT))
                 running_loss = 0.0
+
         writer.add_scalar(f'Loss/train:', epoch_loss / len(data_loader), epoch)
         print(f"[{epoch + 1}] loss: {epoch_loss / len(data_loader):.3f}")
+
     writer.flush()
     return network
+
 
 def test_model(data_loader, network):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -100,75 +112,55 @@ def test_model(data_loader, network):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print('Test accuracy: %.2f %%' % (100 * correct / total))
+    print(F'Test accuracy: {(100 * correct / total):.2f}%')
 
 
-# Fine tuning the model
+# ## Fine-tuning the model
 
-# In[120]:
+# In[ ]:
 
 
-# alexnet = models.alexnet(pretrained=True)
 alexnet = models.alexnet(weights='AlexNet_Weights.DEFAULT')
-
-
-# Add a new fully connected layer to the classifier
-alexnet.classifier.add_module('6', nn.Linear(4096, 10))
-
+alexnet.classifier.add_module('6', nn.Linear(4096, NUM_CLASSES))
 
 optimizer = optim.SGD(alexnet.parameters(), lr=learning_rate, momentum=momentum)
-
-
-# In[121]:
-
-
-
-# Train the model for a few epochs
-trained_net = train_model(train_loader, alexnet,optimizer, loss_function)
 
 
 # In[ ]:
 
 
+trained_network = train_model(train_loader, alexnet, optimizer, loss_function)
+object_to_save = {
+    'model_state_dict': trained_network.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+}
+torch.save(object_to_save, 'alexnet-fine-tuned-cifar-10.pt')
 
 
-
-# In[122]:
-
-
-# Evaluate the model on the test set
-
-test = test_model(test_loader, trained_net)
+# In[ ]:
 
 
-# Feature Extraction
-
-# In[126]:
+test = test_model(test_loader, trained_network)
 
 
-# Load the pre-trained AlexNet model
-# alexnet = models.alexnet(pretrained=True)
+# ## Feature extraction
+
+# In[ ]:
+
+
 alexnet = models.alexnet(weights='AlexNet_Weights.DEFAULT')
-
 
 # Freeze all the layers in the feature extractor
 for param in alexnet.features.parameters():
     param.requires_grad = False
 
-# Add a new fully connected layer to the classifier
-alexnet.classifier.add_module('6', nn.Linear(4096, 10))
+alexnet.classifier.add_module('6', nn.Linear(4096, NUM_CLASSES))
 
 optimizer = optim.SGD(alexnet.parameters(), lr=learning_rate, momentum=momentum)
-# Train the model for a few epochs
-trained_net = train_model(train_loader,alexnet, optimizer, loss_function)
-
-# Evaluate the model on the test set
-
-test = test_model(test_loader, trained_net)
-
-
-# In[ ]:
-
-
-
+trained_network = train_model(train_loader, alexnet, optimizer, loss_function)
+object_to_save = {
+    'model_state_dict': trained_network.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+}
+torch.save(object_to_save, 'alexnet-feature-extracted-cifar-10.pt')
 
