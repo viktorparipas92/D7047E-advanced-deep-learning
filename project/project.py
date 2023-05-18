6c1b4e8eafd9efc4701c48eee17d101cb53e9ebd
Paripás Viktor <viktor.paripas@kognity.com>
2023-05-18 18:47:13 +0200

    Set device to GPU if available

--- project/project.py
+++ project/project.py
@@ -3,8 +3,39 @@
 
 # # Pneumonia Detection with Chest X-ray Images
 
-# In[ ]:
+# ## Setup
+
+# ### Import libraries
+
+# In[1]:
 
 
+import sys
 
+import torch
+import torchvision
+
+
+# ### Using GPU for training if available
+
+# In[2]:
+
+
+print(f'Python version: {sys.version_info.major}.{sys.version_info.minor}')
+print(f'PyTorch version: {torch.__version__}')
+print(f'Torchvision version: {torchvision.__version__}')
+
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    elif torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+        
+    return device
+
+
+device = get_device()
+print(f'Using device: {device}')
 
